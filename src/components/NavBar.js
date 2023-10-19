c3079fcc89e9429427db18c939c38073a8ef23be
JirkaMrk <125826485+JirkaMrk@users.noreply.github.com>
2023-10-19 18:57:03 +0200

    Few fixes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import React from 'react';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 function NavBar() {
   return (
-    <Navbar expand="lg" style={{ fontWeight: 'bold' }}>
+      <Navbar expand="lg" style={{ fontWeight: 'bold' }}>
         <Nav.Link href="/">Home</Nav.Link>
         <Nav.Link href="/ShoppingListPage">My shopping lists</Nav.Link>
          <Navbar.Toggle aria-controls="basic-navbar-nav" />
          <Navbar.Collapse id="basic-navbar-nav">
-        <Nav defaultActiveKey="/home" as="ul">
+         <Nav defaultActiveKey="/home" as="ul">
           <NavDropdown title="Shopping list filter" id="basic-nav-dropdown" >
             <NavDropdown.Item href="#action/3.1">All shopping lists</NavDropdown.Item>
             <NavDropdown.Item href="#action/3.2">
@@ -25,8 +25,23 @@ function NavBar() {
             </NavDropdown.Item>
           </NavDropdown>
           </Nav>
+           </Navbar.Collapse >
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+               <Nav defaultActiveKey="/home" as="ul">
+               <NavDropdown title="User switch" id="basic-nav-dropdown" >
+               <NavDropdown.Item href="#action/3.1">Jirka</NavDropdown.Item>
+               <NavDropdown.Item href="#action/3.2">Karel</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Matěj</NavDropdown.Item>
+               <NavDropdown.Divider />
+               <NavDropdown.Item href="#action/3.4">
+                 Owner
+            </NavDropdown.Item>
+          </NavDropdown>
+          </Nav>
       </Navbar.Collapse>
-  </Navbar>
+
+      </Navbar>
   );
 }
 
