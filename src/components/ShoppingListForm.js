410c538a4c525e6f63334ca343ad45b6edbf0cd1
JirkaMrk <125826485+JirkaMrk@users.noreply.github.com>
2023-11-04 12:23:31 +0100

    Vývoj a úpravy

--- src/components/ShoppingListForm.js
+++ src/components/ShoppingListForm.js
@@ -5,10 +5,11 @@ import { Button, Modal, Form, Row } from "react-bootstrap";
 
 
 const VYCHOZI_KNIHA = [ // výchozí data
-    {id: "1", name: "kniha1", autor: "Jirka", rok: "2023"},
-    {id: "2", name: "kniha2", autor: "Tom", rok: "2020"},
-    {id: "3", name: "kniha3", autor: "Jirka", rok: "2023"},
-    {id: "4", name: "kniha4", autor: "Tom", rok: "2020"}             
+    {id: "0", done:"1", name: "knih0", author: "Jirka", year: "2023"},
+    {id: "1", done:"0", name: "kn1", author: "Jirka", year: "2023"},
+    {id: "2", done:"1", name: "knha2", author: "Tom", year: "2020"},
+    {id: "3", done:"0",name: "knih3", author: "Jirka", year: "2023"},
+    {id: "4", done:"1", name: "niha4", author: "Tom", year: "2020"}             
 ];
 
 function ShoppingListForm(props) {
@@ -23,56 +24,30 @@ function ShoppingListForm(props) {
         })
     };
 
-    
+    function handleHide(id) { // funkce pro smazání položky
+        setVychoziKniha(([...momentlaniList]) => {  // vytvoří nový seznam, který obsahuje všechny položky z původního seznamu
+            const index = momentlaniList.findIndex((item) => item.id === id);   // najde index položky, kterou chceme smazat  
+            momentlaniList.splice(index, 1);    // smaže položku ze seznamu 
+            return momentlaniList;  // vrátí nový seznam
+        })
+    };
+
     const handleSubmit = (event) => {
     event.preventDefault();
     console.log(event);
+
   };
-
-
-
     return (
         <div>
             <h1>
                 Knihovna
             </h1>   
-            <Button variant="primary" onClick= {() => setModalOpen(true)}>
-                +
-            </Button>
-
-            <Modal key={modalOpen} onSubmit={handleSubmit} show={modalOpen} onHide={() => setModalOpen(false)}>
-                <Modal.Header closeButton>
-                    <Modal.Title>
-                        Přidat knihu
-                    </Modal.Title> 
-                    <Modal.Body >
-                        <Form>
-                           <Form.Control
-                                name= "name"
-                                label= "name"
-                    
-                                />
-                           <Form.Control
-                                name= "autor"
-                                label= "autor"
-      
-                                />
-                        </Form>
-                        </Modal.Body>  
-                        <Modal.Footer>
-                            <Button variant="secondary" onClick={() => setModalOpen(false)}>
-                                Zrušit
-                            </Button>
-                            <Button variant="primary" type="submit" onClick={handleSubmit}>
-                                Uložit
-                            </Button> 
-                        </Modal.Footer>
-                </Modal.Header>             
-                </Modal>
-            
             {vychoziKniha.map((item) =>(        
                 <Book key= {item.id} {...item} onDelete={(id) => handleDelete(item.id)}/>            
             ))}
+            <Button variant="primary" onClick= {() => setModalOpen(true)}>
+                +
+            </Button>
          
         </div>
         
--- src/components/book.js
+++ src/components/book.js
@@ -1,16 +1,43 @@
 import ListGroup from 'react-bootstrap/ListGroup';
 import Button from 'react-bootstrap/Button';
+import { FormControl, Row, Form, Col } from 'react-bootstrap';
 
 function Book(props) {
     return (
-        <ListGroup.Item className="my-1">
-             {props.name}
-             {props.autor}
-             {props.rok}
-            <Button variant="danger" onClick={props.onDelete}>
-                X
-            </Button>
-        </ListGroup.Item>
+        <ListGroup id={props}>  
+        
+            <Row className="align-items-center">
+                        <Col sm={1} className="my-1">
+                          <Form.Check
+                            type='checkbox'
+                            name='checkBox'
+                            label="OK"
+                            value={props.done}
+                         />
+                        </Col>
+                        <Col sm={6} className="my-1">
+                            <FormControl
+                            value={props.name}
+                            />
+                        </Col>
+                        <Col sm={2} className="my-1">
+                            <FormControl
+                            value={props.author}
+                            />
+                        </Col>
+                        <Col sm={2} className="my-1">
+                            <FormControl
+                            value={props.year}
+                            />
+                        </Col>
+                        <Col sm={1} className="my-1">
+                        <Button variant="danger" onClick={props.onDelete}>
+                           X
+                        </Button>
+                        </Col>
+             </Row>
+        </ListGroup> 
+
     );
 }
 
