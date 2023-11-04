579d2d33551cb3d13aa6c22cec775bd319ef6765
JirkaMrk <125826485+JirkaMrk@users.noreply.github.com>
2023-11-04 13:25:50 +0100

    13:25

--- src/components/ShoppingListForm.js
+++ src/components/ShoppingListForm.js
@@ -5,6 +5,8 @@ import { Button, Modal, Form, Row } from "react-bootstrap";
 
 
 const VYCHOZI_KNIHA = [ // výchozí data
+
+
     {id: "0", done:"1", name: "knih0", author: "Jirka", year: "2023"},
     {id: "1", done:"0", name: "kn1", author: "Jirka", year: "2023"},
     {id: "2", done:"1", name: "knha2", author: "Tom", year: "2020"},
@@ -15,6 +17,8 @@ const VYCHOZI_KNIHA = [ // výchozí data
 function ShoppingListForm(props) {
     const [ modalOpen, setModalOpen ] = useState(false); // výchozí data zavření modálního okna
     const [vychoziKniha, setVychoziKniha] = useState(VYCHOZI_KNIHA);    // výchozí data  
+    const [showChecked, setShowChecked] = useState(true); // výchozí data zobrazení zaškrtnutých položek
+    
     
     function handleDelete(id) { // funkce pro smazání položky
         setVychoziKniha(([...momentlaniList]) => {  // vytvoří nový seznam, který obsahuje všechny položky z původního seznamu
@@ -23,31 +27,35 @@ function ShoppingListForm(props) {
             return momentlaniList;  // vrátí nový seznam
         })
     };
-
-    function handleHide(id) { // funkce pro smazání položky
-        setVychoziKniha(([...momentlaniList]) => {  // vytvoří nový seznam, který obsahuje všechny položky z původního seznamu
-            const index = momentlaniList.findIndex((item) => item.id === id);   // najde index položky, kterou chceme smazat  
-            momentlaniList.splice(index, 1);    // smaže položku ze seznamu 
-            return momentlaniList;  // vrátí nový seznam
-        })
-    };
-
+/*
     const handleSubmit = (event) => {
     event.preventDefault();
     console.log(event);
+    */
 
+    function handleToggleShowChecked() { {
+        setShowChecked(!showChecked);
+    };
   };
+
     return (
         <div>
             <h1>
-                Knihovna
-            </h1>   
-            {vychoziKniha.map((item) =>(        
-                <Book key= {item.id} {...item} onDelete={(id) => handleDelete(item.id)}/>            
-            ))}
+                Můj shopping list
+            </h1>  
+            {vychoziKniha
+                    .filter((item) => showChecked || item.done === "0")
+                    .map((item) => (
+                        <Book key={item.id} {...item} onDelete={() => handleDelete(item.id)} />
+                    ))} 
             <Button variant="primary" onClick= {() => setModalOpen(true)}>
                 +
             </Button>
+            
+                <Button variant="primary" onClick={handleToggleShowChecked}>
+                    {showChecked ? "Hide checked" : "Show checked"}
+                </Button>
+               
          
         </div>
         
@@ -56,3 +64,10 @@ function ShoppingListForm(props) {
 }
 
 export default ShoppingListForm;
+
+ // {vychoziKniha.map((item) =>(        
+ //           <Book key= {item.id} {...item} onDelete={(id) => handleDelete(item.id)}/>            
+  //          ))}
+  //          */
+
+
--- src/components/book.js
+++ src/components/book.js
@@ -1,33 +1,41 @@
 import ListGroup from 'react-bootstrap/ListGroup';
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import { FormControl, Row, Form, Col } from 'react-bootstrap';
 
+
+
 function Book(props) {
+
     return (
         <ListGroup id={props}>  
         
             <Row className="align-items-center">
                         <Col sm={1} className="my-1">
-                          <Form.Check
-                            type='checkbox'
-                            name='checkBox'
-                            label="OK"
-                            value={props.done}
+                            <Form.Check
+                                type='checkbox'
+                                name='checkBox'
+                                label="OK"
+                                defaultChecked={props.done === "1" ? true : false }
+                                onChange={props.onToggleDone}
                          />
                         </Col>
                         <Col sm={6} className="my-1">
                             <FormControl
                             value={props.name}
+                            disabled={props.done === "1" ? true : false}
                             />
                         </Col>
                         <Col sm={2} className="my-1">
                             <FormControl
                             value={props.author}
+                            disabled={props.done === "1" ? true : false}
                             />
                         </Col>
                         <Col sm={2} className="my-1">
                             <FormControl
                             value={props.year}
+                            disabled={props.done === "1" ? true : false}
                             />
                         </Col>
                         <Col sm={1} className="my-1">
@@ -40,5 +48,6 @@ function Book(props) {
 
     );
 }
+
 
 export default Book;
